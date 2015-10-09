af41296d8a5a8c35a0a252e91d8f9860ad530e62
DelphiRace <delphi.race@gmail.com>
2015-10-09 19:29:40 +0800

    Facebook OAuth

--- public/include/js/config/config.js
+++ public/include/js/config/config.js
@@ -1,4 +1,5 @@
 var configObject = {
     "GoogleSignInBack" : "http://127.0.0.1:99/GoogleOAuthSignin",
+    "FacebookSignInBack" : "http://127.0.0.1:99/FacebookOAuthSignin",
     "OAuthUrl": "http://127.0.0.1:99/login",
 };
---
+++ public/include/js/facebook_oauth/facebook_oauth.js
@@ -0,0 +1,43 @@
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '1249539155071484',
+		xfbml      : true,
+		version    : 'v2.5'
+	});  
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkFacebookLoginState() {
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			// Logged into your app and Facebook.
+			//console.log(response);
+			sendFacebookAccoundInfo(response);
+		}else{
+			alert('Can Not Login Your Facebook, Please Try Again!');
+		}
+	  //testAPI();
+	});
+}
+
+function sendFacebookAccoundInfo(response){
+    $.ajax({
+           url: configObject.FacebookSignInBack,
+           type: "POST",
+           data: response,
+           dataType: 'JSON',
+           async: false,
+           success: function(rs){
+			   //console.log(rs);
+               redirectPage(rs);
+           }
+    });
+}
